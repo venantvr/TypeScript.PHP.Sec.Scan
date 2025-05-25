f3f98ff86a3ee612ecd76ece32e03a21ec4c6a40
rvv <r.venant.valery@gmail.com>
2025-05-25 12:09:46 +0200

    Refactoring...

--- tests/taintTracker.test.ts
+++ tests/taintTracker.test.ts
@@ -56,4 +56,26 @@ describe('TaintAnalyzer', () => {
         expect(taintFlow).to.include("Variable '$id' assigned from source '$_GET['id']'");
         expect(taintFlow).to.include("Variable '$id' passed as parameter to function 'some_function'");
     });
+
+    it('devrait détecter une variable tainted passée à une fonction après 2 affectations', () => {
+        const code = `
+            <?php
+            $tmp = $_GET['id'];
+            $id = $tmp;
+            some_function($id);
+            ?>
+        `;
+        const tree = parser.parse(code);
+        const parserInstance = new SyntaxTreeParser(Buffer.from(code, 'utf-8'), tree, 'test.php');
+        const events = parserInstance.parse();
+        const analyzer = new TaintAnalyzer(rules, 'test.php');
+        const vulnerabilities = analyzer.analyze(events);
+        expect(vulnerabilities).to.have.lengthOf(2); // 1 avertissement pour source
+        expect(vulnerabilities.some(v => v.type === 'unsanitized_source' && v.variable === '$id' && v.severity === 'warning')).to.be.true;
+
+        const taintFlow = analyzer.printTaintFlow();
+        expect(taintFlow).to.include("Variable '$tmp' assigned from source '$_GET['id']'");
+        expect(taintFlow).to.include("Variable '$id' assigned from source '$tmp'")
+        expect(taintFlow).to.include("Variable '$id' passed as parameter to function 'some_function'");
+    });
 });
